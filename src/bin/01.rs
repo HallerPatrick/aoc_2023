117baf1476e344498a423d314c55b0535c2f4d6c
Patrick Haller <patrickhaller40@googlemail.com>
2023-12-02 17:33:37 +0100

    Solved day1 and day2

---
+++ src/bin/01.rs
@@ -0,0 +1,101 @@
+advent_of_code::solution!(1);
+
+use std::collections::HashMap;
+
+use fancy_regex::Regex;
+
+fn extract_first_and_list_digit(s: &str) -> u32 {
+    let mut integers: Vec<char> = Vec::new();
+    for char in s.chars().into_iter() {
+        if char.is_numeric() {
+            integers.push(char);
+        }
+    }
+
+    if let Some(first_digit) = integers.first() {
+        if let Some(last_digit) = integers.last() {
+            let str_repr = format!("{}{}", first_digit, last_digit);
+            if let Ok(value) = str_repr.parse::<u32>() {
+                return value
+            }
+        }
+
+    }
+    0
+}
+
+fn extract_first_and_list_digit_with_string_value(s: &str, m: &HashMap<&str, &str>) -> u32 {
+    let re_digit_or_str_digit = Regex::new(r"(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))").unwrap();
+
+    let mut digits: Vec<&str> = Vec::new();
+
+    for capture in re_digit_or_str_digit.captures_iter(s).map(|x| x.unwrap()) {
+        digits.push(capture.get(1).expect("Not Found").as_str());
+    }
+
+    if let Some(first_digit_str) = digits.first() {
+        if let Some(last_digit_str) = digits.last() {
+            let first_digit: &str;
+            let last_digit: &str;
+            if m.contains_key(first_digit_str) {
+                first_digit = m.get(first_digit_str).unwrap();
+            } else {
+                first_digit = first_digit_str;
+            }
+
+            if m.contains_key(last_digit_str) {
+                last_digit = m.get(last_digit_str).unwrap();
+            } else {
+                last_digit = last_digit_str;
+            }
+            let str_repr = format!("{}{}", first_digit, last_digit);
+            println!("{}", str_repr);
+            if let Ok(value) = str_repr.parse::<u32>() {
+                return value
+            }
+        }
+
+    }
+
+    0
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let result: u32 = input.split("\n").into_iter().map(extract_first_and_list_digit).sum();
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+
+    let str_to_digit = HashMap::from([
+        ("one",   "1"),
+        ("two",   "2"),
+        ("three",  "3"),
+        ("four",  "4"),
+        ("five",  "5"),
+        ("six",   "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine",  "9"),
+        ("zero",  "0")
+    ]);
+    let result: u32 = input.split("\n").into_iter().map(|x| extract_first_and_list_digit_with_string_value(x, &str_to_digit)).sum();
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(54630));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,128 @@
+advent_of_code::solution!(2);
+
+use fancy_regex::Regex;
+
+#[derive(Debug, Copy, Clone)]
+struct Round {
+    blue: u32,
+    red: u32,
+    green: u32
+}
+
+impl Round {
+
+    fn from_str(input: &str) -> Self {
+        let re = Regex::new(r"((?<blue>\d+) (blue))|((?<red>\d+) (red))|((?<green>\d+) (green))").unwrap();
+
+        let mut green = 0;
+        let mut red = 0;
+        let mut blue = 0;
+
+        for captures in re.captures_iter(input).map(|x| x.unwrap()) {
+            if let Some(green_match) = captures.name("green") {
+                green = green_match.as_str().parse::<u32>().unwrap()
+            }
+            if let Some(red_match) =  captures.name("red") {
+                red = red_match.as_str().parse::<u32>().unwrap()
+            }
+            if let Some(blue_match) = captures.name("blue") {
+                blue = blue_match.as_str().parse::<u32>().unwrap()
+            }
+        }
+        Self {green, red, blue}
+    }
+
+}
+
+struct Game {
+    id: u32,
+    rounds: Vec<Round>
+}
+
+fn extract_game_id(input: &str) -> u32 {
+    let re = Regex::new(r"Game (\d+)").unwrap();
+    re.captures(input).unwrap().unwrap().get(1).unwrap().as_str().parse::<u32>().unwrap()
+}
+
+impl Game {
+
+    fn from_str(input: &str) -> Self {
+        let game_id = extract_game_id(input);
+
+        // "Game <id>: "
+        let game_id_offset = 4 + game_id.to_string().len() + 2;
+
+        let rounds_str = &input[game_id_offset..];
+
+        let rounds: Vec<Round> = rounds_str.split(";").into_iter().map(str::trim).map(Round::from_str).collect();
+
+        Self {id: game_id, rounds}
+    }
+
+    fn is_possible(&self, round: &Round) -> bool {
+        for played_round in &self.rounds {
+            if played_round.blue > round.blue {
+                return false;
+            }
+            if played_round.red > round.red {
+                return false;
+            }
+
+            if played_round.green > round.green {
+                return false;
+            }
+
+        }
+        true
+    }
+
+    fn minimal_cubes(&self) -> Round {
+
+        let mut largest_red = 0;
+        let mut largest_green = 0;
+        let mut largest_blue = 0;
+
+        for played_round in &self.rounds {
+
+            if played_round.red > largest_red {
+                largest_red = played_round.red
+            }
+            if played_round.green > largest_green {
+                largest_green = played_round.green
+            }
+            if played_round.blue > largest_blue {
+                largest_blue = played_round.blue
+            }
+
+        }
+
+        Round {red: largest_red, blue: largest_blue, green: largest_green}
+    }
+
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let round = Round { red: 12, green: 13, blue: 14};
+    Some(input.split("\n").into_iter().map(Game::from_str).filter(|x| x.is_possible(&round)).map(|x| x.id).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(input.split("\n").into_iter().map(Game::from_str).map(|x| x.minimal_cubes()).map(|x| x.blue * x.red * x.green).sum())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
